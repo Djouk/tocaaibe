3a7a6136b279118dd4be2568ba8441ccf0a2cf18
Andrey Andreata <andreyctb@gmail.com>
2022-06-25 05:30:34 -0300

    update ok

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,6 @@ router.get('/', (req, res) => res.send('Welcome'));
 router.post('/users', userController.createUser);
 router.get('/users', userController.getAllUsers);
 router.get('/users/:id', userController.getUserById);
+router.put('/users/:id', userController.updateUser);
 
 module.exports = router;
