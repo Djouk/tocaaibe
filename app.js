43642c2fbc99d3f7d915f4851e049e07eefe2ff5
Andrey Andreata <andreyctb@gmail.com>
2022-06-25 03:15:49 -0300

    nodemon ok

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const a = 2;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-if (a == 3) {
-    console.log('oi');
-}
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const db = require('./app/models');
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to application.' });
+});
+require('./app/routes/turorial.routes')(app);
+// set port, listen for requests
+const PORT = 4568;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
